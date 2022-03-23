480aefdf47833802276cd20b978c9ba4651ce432
Ubaidullah92 <ms.ubaidullah@gmail.com>
2022-03-23 09:01:52 +0530

    first commit

--- src/fileStore.js
+++ src/fileStore.js
@@ -13,17 +13,13 @@ let {
 } = argv;
 
 const firstReq = `https://cataas.com/cat/says/${greeting}?width=${width}&height=${height}&color=${color}&s=${size}`;
+console.log(firstReq);
 const secondReq = `https://cataas.com/cat/says/${who}?width=${width}&height=${height}&color=${color}&s=${size}`;
 
 async function compositeImages(image1, image2) {
   try {
-    const data = await sharp()
+    const data = await sharp(image1)
       .composite([
-        {
-          input: image1,
-          top: 0,
-          left: 0,
-        },
         {
           input: image2,
           top: 0,
@@ -41,13 +37,13 @@ async function compositeImages(image1, image2) {
 axios
   .get(firstReq)
   .then((res1) => {
-    // console.log("res1", res1);
+    // console.log("res1", Buffer.from(res1.data).toString("base64"));
     axios
       .get(secondReq)
       .then((res2) => {
         compositeImages(
-          Buffer.from(res1.data, "binary").toString("base64"),
-          Buffer.from(res2.data, "binary").toString("base64")
+          Buffer.from(res1.data, "binary"),
+          Buffer.from(res2.data, "binary")
         );
       })
       .catch((error) => {
